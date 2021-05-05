a520e2d383af9c170a3b8fc6387406a92d040d43
Oskari Tiala <oskari.tiala@haltu.fi>
2021-05-05 13:24:25 +0300

    Replicate accessToken fix in HobbyEditView

--- src/components/views/HobbyEditView.jsx
+++ src/components/views/HobbyEditView.jsx
@@ -3,20 +3,23 @@ import { useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Typography, Container } from '@material-ui/core';
 import ActionCreators from '../../actions';
-import { useDeepCompareEffect } from '../../hooks';
+import { useDeepCompareEffect, useSelector } from '../../hooks';
 import HobbyEditForm from '../blocks/HobbyEditForm';
 
 const HobbyEditView = props => {
   const dispatch = useDispatch();
+  const authState = useSelector(state => state.auth);
   const { id } = props.match.params;
   useDeepCompareEffect(() => {
-    // initial data fetch
-    dispatch(ActionCreators.fetchCategories());
-    dispatch(ActionCreators.fetchOrganizers());
-    dispatch(ActionCreators.fetchLocations());
-    dispatch(ActionCreators.fetchHobby(id));
-    dispatch(ActionCreators.fetchHobbyEvents(id));
-  }, []);
+    if (authState.accessToken) {
+      // initial data fetch
+      dispatch(ActionCreators.fetchCategories());
+      dispatch(ActionCreators.fetchOrganizers());
+      dispatch(ActionCreators.fetchLocations());
+      dispatch(ActionCreators.fetchHobby(id));
+      dispatch(ActionCreators.fetchHobbyEvents(id));
+    }
+  }, [authState.accessToken]);
 
   return (
     <Container maxWidth="sm">
